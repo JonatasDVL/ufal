d15150eba88b8ef93f7ce594b9aeb483d0441d18
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-08-29 16:42:00 -0300

    Up

---
+++ algoritimos-e-programacao-de-computadores/aula07/cda/cda002.py
@@ -0,0 +1,14 @@
+telefones = {'jose': 2545, 'pedro': 5214, 'ana': 5522}
+for key in telefones:
+	print(key)
+for key in telefones:
+	print(key, telefones[key])
+
+
+# telefones = [0,1,2,4]
+# agendaTelefonica = {}
+# agendaTelefonica["A"] = telefones
+# telefones = []
+# telefones = agendaTelefonica["A"]
+
+# print(telefones)
--- algoritimos-e-programacao-de-computadores/aula07/exercicios/ex003.py
+++ algoritimos-e-programacao-de-computadores/aula07/exercicios/ex003.py
@@ -6,10 +6,108 @@
  Exercício 03:
      3. Escreva um programa para armazenar uma agenda de telefones em um dicionário. Cada pessoa pode ter um ou mais telefones e a chave do dicionário é o nome da pessoa. Seu programa deve ter as seguintes funções:
 
-        incluirNovoNome – essa função acrescenta um novo nome na agenda, com um ou mais telefones. Ela deve receber como argumentos o nome e os telefones. ­ 
+        incluirNovoNome - essa função acrescenta um novo nome na agenda, com um ou mais telefones. Ela deve receber como argumentos o nome e os telefones. 
 
-        incluirTelefone – essa função acrescenta um telefone em um nome existente na agenda. Caso o nome não exista na agenda, você deve perguntar se a pessoa deseja incluí­lo. Caso a resposta seja afirmativa, use a função anterior para incluir o novo nome. ­ excluirTelefone – essa função exclui um telefone de uma pessoa que já está na agenda. Se a pessoa tiver apenas um telefone, ela deve serexcluída da agenda. ­ 
+        incluirTelefone - essa função acrescenta um telefone em um nome existente na agenda. Caso o nome não exista na agenda, você deve perguntar se a pessoa deseja incluílo. Caso a resposta seja afirmativa, use a função anterior para incluir o novo nome. 
         
-        excluirNome – essa função exclui uma pessoa da agenda. ­ 
+        excluirTelefone - essa função exclui um telefone de uma pessoa que já está na agenda. Se a pessoa tiver apenas um telefone, ela deve ser excluída da agenda.  
+        
+        excluirNome - essa função exclui uma pessoa da agenda. 
+
+        consultarTelefone - essa função retorna os telefones de uma pessoa na agenda. 
 """
 
+def incluirNovoNome(agendaTelefonica, nome=""):
+    if nome == "":
+        nome = input("Digite o nome do novo contato: ") 
+    telefones = []
+    while not telefones or resposta == "S": 
+        numeroTelefone = input(f"Digite o número que deseja adicionar no contato de {nome}: ")
+        ##fazer uma função de verificador de numero 
+        telefones.append(numeroTelefone)
+        resposta = input("Você quer adicionar mais um número(s/n)").upper()
+    
+    agendaTelefonica[nome] = telefones
+    return agendaTelefonica
+
+def incluirTelefone(agendaTelefonica):
+    nome = input("Digite o nome do contato: ") 
+    resposta2 = "N"
+    if len(agendaTelefonica) != 0:
+        for chave in agendaTelefonica:
+            resposta1 = "S"
+            if chave == nome:
+                resposta1 = "N"
+                break
+    else:
+        resposta1 = "S"
+    if resposta1 == "S":
+        resposta2 = input("Você quer adicionar esse novo contato na sua agenda(s/n): ").upper()
+    if resposta2 == "S":
+        agendaTelefonica = incluirNovoNome(agendaTelefonica, nome)
+        return agendaTelefonica
+    else:
+        telefones = agendaTelefonica[nome]
+        resposta = "S"
+        while resposta == "S": 
+            numeroTelefone = input(f"Digite o número que deseja adicionar no contato de {nome}: ")
+            ##fazer uma função de verificador de numero 
+            telefones.append(numeroTelefone)
+            resposta = input("Você quer adicionar mais um número(s/n): ").upper()
+        agendaTelefonica[nome] = telefones
+        return agendaTelefonica
+
+def excluirTelefone(agendaTelefonica):
+    nome = input("Digite o nome do contato: ") 
+    if len(agendaTelefonica) != 0:
+        for chave in agendaTelefonica:
+            resposta = "N"
+            if chave == nome:
+                resposta = "S"
+                break
+    else:
+        resposta = "N"
+    if resposta == "S":
+        while resposta == "S":
+            print(f"{nome}: |",end="")
+            for elemento in agendaTelefonica[nome]:
+                print(f"{elemento}|",end="")
+            telefone = input("\nDigite o número de telefone que deseja excluir: ")
+            for i in range (len(agendaTelefonica[nome])):
+                if(agendaTelefonica[nome][i] == telefone):
+                    del agendaTelefonica[nome][i]
+                    break
+            if len(agendaTelefonica[nome]) == 0:
+                del agendaTelefonica[nome] 
+                resposta = "N"
+            else:
+                resposta = input("Você deseja continuar a excluir números telefonicos desse contatos(s/n): ").upper()
+    else:   
+        print("Esse contanto não existe.")
+    return agendaTelefonica
+
+def excluirNome(agendaTelefonica):
+    nome = input("Digite o nome do contato que deseja excluir: ") 
+    return agendaTelefonica
+
+def consultarTelefone(agendaTelefonica):
+    nome = input("Digite o nome do contato que deseja excluir: ") 
+    return agendaTelefonica
+
+agendaTelefonica = {}
+opcao = "a"
+
+while opcao != "":
+    opcao = input(f"\n1.Para incluir um novo contato; \n2.Para incluir um novo telefone a um contato já existente; \n3.Para excluir um número de um contato já existente; \n4.Para excluir um contato;\n5.Para consultar um telefone;\n\nEscolha uma das opções descrita acima: ")
+    if opcao == "1":
+        agendaTelefonica = incluirNovoNome(agendaTelefonica)
+    elif opcao == "2":
+        agendaTelefonica = incluirTelefone(agendaTelefonica)
+    elif opcao == "3":
+        agendaTelefonica = excluirTelefone(agendaTelefonica)    
+    elif opcao == "4":
+        agendaTelefonica = excluirNome(agendaTelefonica)
+    elif opcao == "5":
+        agendaTelefonica = consultarTelefone(agendaTelefonica)
+
+print(agendaTelefonica)
