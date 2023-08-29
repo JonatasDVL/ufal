ff7087653f6c19a21f00698e9b97ed7cf7300e11
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-08-29 12:25:33 -0300

    up

---
+++ algoritimos-e-programacao-de-computadores/aula07/cda/cda01.py
@@ -0,0 +1,25 @@
+# Crie um conjunto de cores com nome e numero rgb e um conjunto de estilos musicais 
+
+conjuntoGeral = []
+
+conjuntosCores = {("Azul",(0,0,255)),("Vermelho",(0,255,0)),("Verde",(255,0,0))}
+conjuntosMusicas = {"Rock", "Funk", "Gospel", "Axé", "Sertanejo", "Forró"}
+conjuntoGeral.append(conjuntosCores)
+conjuntoGeral.append(conjuntosMusicas)
+
+print(conjuntoGeral)
+
+# conjuntoGeral = []
+# continuar = "s"
+
+# while continuar == "s": 
+#     nome = input("Digite um nome de uma cor:")
+#     numeracao = input("Digite o rgb da cor escrita anteriormente:")
+#     conjuntosCores = nome, numeracao
+#     nome = input("Digite um nome de um estilo musical:")
+#     conjuntoMusicas = nome
+#     continuar = input("Você deseja continuar a digita(s/n):")
+#     conjuntoGeral.append(conjuntosCores)
+#     conjuntoGeral.append(conjuntoMusicas)
+
+# print(conjuntoGeral)
--- algoritimos-e-programacao-de-computadores/aula07/exercicios/ex001.py
+++ algoritimos-e-programacao-de-computadores/aula07/exercicios/ex001.py
@@ -6,3 +6,17 @@
  Exercício 01:
      1. Escreva uma função que conta a quantidade de vogais em um texto e armazena tal quantidade em um dicionário, onde a chave é a vogal considerada.
 """
+
+def lerQuantidadeVogais(frase):
+    vogais = "aeiou"
+    frase.lower()
+    quantidadeVogais = {}
+    quantidadeVogais = [("A:", 0), ("B:", 0), ("C:", 0), ("D:", 0), ("E:", 0)]
+    for i in range(len(frase)):
+        for j in (len(vogais)):
+            if frase[i] == vogais[j]: 
+                quantidadeVogais[j][1] += + 1
+    return quantidadeVogais 
+
+frase = "Ana Subiu o pé de feijão."
+print(lerQuantidadeVogais(frase))
