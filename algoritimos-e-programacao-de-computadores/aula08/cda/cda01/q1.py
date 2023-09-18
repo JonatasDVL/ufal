154aeab2ae7b0679b75e918e1bc9bf4ddaa13105
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-09-18 15:23:38 -0300

    up

--- algoritimos-e-programacao-de-computadores/aula08/cda/cda01/q1.py
+++ algoritimos-e-programacao-de-computadores/aula08/cda/cda01/q1.py
@@ -1,16 +1,14 @@
 arquivo = open(r"C:\Users\jonat\OneDrive\Documentos\GitHub\ufal\algoritimos-e-programacao-de-computadores\aula08\cda\cda01\dados.txt")
-texto = arquivo.read()
+lista = []
+for linha in arquivo.readlines():
+    linha = linha.replace("\n","")
+    lista.append(linha)
 arquivo.close()
-print(texto)
-
-texto = texto.split("\n")
-print(texto)
 
 usuarios = []
 
-for elemento in texto:
+for elemento in lista:
     atributo = elemento.split(", ")
     usuario = {"nome":atributo[0], "email":atributo[1], "senha":atributo[2]}
     usuarios.append(usuario)
-
 print(usuarios)
