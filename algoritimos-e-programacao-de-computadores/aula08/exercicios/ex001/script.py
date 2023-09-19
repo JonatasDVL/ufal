e74bae1ee87770f33f1a0555fe8f845b3ced14df
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-09-19 10:28:09 -0300

    1 questão

--- algoritimos-e-programacao-de-computadores/aula08/exercicios/ex001/script.py
+++ algoritimos-e-programacao-de-computadores/aula08/exercicios/ex001/script.py
@@ -4,7 +4,20 @@
 #     3235574 Mariana
 # o programa deve gerar um dicionário no qual as chaves são as identidades e os valores os nomes. Ao final o programa deve exibir o dicionário
 
+dados = []
 
-arquivo = open(r"C:\Users\jonat\OneDrive\Documentos\GitHub\ufal\algoritimos-e-programacao-de-computadores\aula08\exercicios\ex001\arquivo.txt", "w")
-arquivo.write("")
+arquivo = open(r"C:\Users\jonat\OneDrive\Documentos\GitHub\ufal\algoritimos-e-programacao-de-computadores\aula08\exercicios\ex001\arquivo.txt", "r")
+for linha in arquivo.readlines():
+    linha = linha.replace("\n","")
+    dados.append(linha)
 arquivo.close()
+
+print(dados)
+
+dicionario = {}
+for atributos in dados:
+    atributo = atributos.split(" ")
+    dicionarioTemp = {atributo[0]:atributo[1]}
+    dicionario |= dicionarioTemp
+
+print(dicionario)
