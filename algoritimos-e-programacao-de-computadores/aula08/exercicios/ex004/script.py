8f163a66d29e1c8c1d0ce79d5f4df6679561ac86
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-09-20 09:41:13 -0300

    up

--- algoritimos-e-programacao-de-computadores/aula08/exercicios/ex004/script.py
+++ algoritimos-e-programacao-de-computadores/aula08/exercicios/ex004/script.py
@@ -1,5 +1,26 @@
 # 4. Faça um programa que leia a temperatura média de cada mês do ano em um arquivo e armazene-as em uma lista. Em seguida, calcule a média anual das temperaturas e mostre a média calculada juntamente com todas as temperaturas acima da média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 - Janeiro, 2 - Fevereiro, . . . ).
 
+def calculaMes(numero):
+    meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    return meses[numero - 1]
+
 arquivo = open(r"ex004\arquivo.txt", "r")
-arquivo.readline()
+temperaturas = []
+for linha in arquivo.readlines():
+    linha = linha.replace("\n", "")
+    temperaturas.append(linha)
 arquivo.close()
+
+media = 0
+i = 0
+print("Temperaturas por mês:")
+for temperatura in temperaturas:
+    i += 1
+    mes = calculaMes(i)
+    print(f"{mes}: {temperatura}°C")
+    media += float(temperatura)
+    if i == 12:
+        i = 0
+
+media = media / len(temperaturas)
+print(f"Média anual das temperaturas: {media:.2f}°C")    
