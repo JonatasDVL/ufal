ef5dcc37b64e432390ea2597939dc124ca2ce02e
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-09-20 09:28:51 -0300

    up

--- algoritimos-e-programacao-de-computadores/aula08/exercicios/ex003/script.py
+++ algoritimos-e-programacao-de-computadores/aula08/exercicios/ex003/script.py
@@ -5,44 +5,43 @@
 #     Calcule e mostre a média das notas.
 #     Calcule e mostre a quantidade de notas acima da média calculada.
 
-# def mostrarQuantidadeNotas(notas):
-#      quantidade = len(notas)
-#      return quantidade
+def exibirNotas(listasNotas):
+    print("| AB1   | AB2   | SOMA  | MÉDIA |\n+-------+-------+-------+-------+")
+    contador = 0
+    quantidade = 0
+    for notas in listasNotas:
+        notas = notas.split(" ")
+        for nota in notas:
+            print(f"| {nota:<5} ", end="")
+            quantidade += 1
+        print("|")
+        quantidade -= 2
+        if float(nota) >= 7:
+            contador += 1
+    print(f"Quantidade de notas lidas: {quantidade}")
+    print(f"Quantidade de notas acima da média: {contador}\n") 
+    return 
 
-# def exibirNotas(notas):
-#     print(notas)
-#     return 
-
-# def calcularSoma(nota1, nota2):    
-#     soma = nota1 + nota2
-#     return soma
-
-# def calcularMedia(nota1, nota2, somas=None):
-#     if somas == None:
-#         somas = calcularSoma(nota1, nota2)
-#     media = somas/2
-#     return media
-
-# def calcularMostrarQuantidadeAcimaMedia():
-
-#     return 
-
-# def adicionarDadosArquivo():
-#     arquivo = open(r"ex003\arquivo.txt", "a")
-#     resposta = "s"
-#     while resposta == "s":
-#         nota1 = float(input("Digite a primeira nota: "))
-#         nota2 = float(input("Digite a segunda nota: "))
-#         soma = 
-#         arquivo.append(nota)
-#     arquivo.close()
-
+def adicionarDadosArquivo():
+    arquivo = open(r"ex003\arquivo.txt", "a")
+    resposta = "s"
+    while resposta == "s":
+        nota1 = float(input("Digite a primeira nota: "))
+        nota2 = float(input("Digite a segunda nota: "))
+        soma = nota1 + nota2
+        media = soma/2
+        arquivo.write(f"{nota1} {nota2} {soma} {media}\n")
+        resposta = input("Digite (s) se desejas continuar: ") 
+    arquivo.close()
+    notas = lerArquivos()
+    return notas
 
 def removerAdicionarDadosArquivo():
     arquivo = open(r"ex003\arquivo.txt", "w")
-    arquivo.write()
+    arquivo.write("")
     arquivo.close()
-
+    notas = adicionarDadosArquivo()
+    return notas
 
 def lerArquivos():
     arquivo = open(r"ex003\arquivo.txt", "r")    
@@ -56,16 +55,16 @@ def inicio():
     notas = lerArquivos()
     resposta = "s"    
     while resposta == "s":
-        opcao = input("Opções do Sistema de Notas:\n1.Adicionar notas no sistema; \n2.Exibir as notas do sistema; \n3.Remover os dados anteriores e adicionar novas notas no sistema; \n4.Fechar o sistema; \nDigite o número da opção que desejas: ")
+        opcao = input("Opções do Sistema de Notas:\n1.Adicionar notas no sistema; \n2.Exibir as notas do sistema; \n3.Remover os dados anteriores e adicionar novas notas no sistema; \n4.Fechar e salvar o sistema; \nDigite o número da opção que desejas: ")
         if opcao == "1":
-            print("Você escolheu a opcao número 1.")
-            
+            print("Você escolheu a opcao número 1.\n")
+            notas = adicionarDadosArquivo()
         elif opcao == "2":
-            print("Você escolheu a opcao número 2.")
-
+            print("Você escolheu a opcao número 2.\n")
+            exibirNotas(notas)
         elif opcao == "3":
-            print("Você escolheu a opcao número 3.")
-            notas = removerAdicionarDadosArquivo(notas)
+            print("Você escolheu a opcao número 3.\n")
+            notas = removerAdicionarDadosArquivo()
         elif opcao == "4":
             resposta = "n"
             print("Você fechou o programa.")
--- algoritimos-e-programacao-de-computadores/aula08/exercicios/ex004/script.py
+++ algoritimos-e-programacao-de-computadores/aula08/exercicios/ex004/script.py
@@ -1,5 +1,5 @@
 # 4. Faça um programa que leia a temperatura média de cada mês do ano em um arquivo e armazene-as em uma lista. Em seguida, calcule a média anual das temperaturas e mostre a média calculada juntamente com todas as temperaturas acima da média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 - Janeiro, 2 - Fevereiro, . . . ).
 
-arquivo = open(r"ex004\arquivo.txt", "w")
-arquivo.write("")
+arquivo = open(r"ex004\arquivo.txt", "r")
+arquivo.readline()
 arquivo.close()
