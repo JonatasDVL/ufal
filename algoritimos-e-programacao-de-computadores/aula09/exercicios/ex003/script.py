9006b1a2bcafc195f5978db93bab279e2a860759
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-10-01 14:43:39 -0300

    up

--- algoritimos-e-programacao-de-computadores/aula09/exercicios/ex003/script.py
+++ algoritimos-e-programacao-de-computadores/aula09/exercicios/ex003/script.py
@@ -12,39 +12,69 @@ def escreverDadosArquivos(dados, caminho):
     df.to_csv(caminho, index=False)
     return
 
+def inserirDadosArquivos(dados, caminho):
+    df = pd.DataFrame(dados)
+    df.to_csv(caminho, index=False)
+    return
+
 def lerDadosArquivos(caminho):
     dados = pd.read_csv(caminho)
     return dados
 
-def buscarDadosArquivos(caminho, nome):
+# def buscarDadosArquivos(caminho, nome):
+#     dados = lerDadosArquivos(caminho)
+#     resultado = ""
+#     for i in range(len(dados["Nome"])):
+#         if dados["Nome"][i] == nome:
+#             for chave in dados.keys():
+#                 resultado += f"{dados[chave][i]} "
+#             return resultado
+#     resultado = "Não existe contato com esse nome."
+#     return resultado
+
+def buscarContatoNome(caminho, nome):
     dados = lerDadosArquivos(caminho)
-    resultado = ""
-    for i in range(len(dados["Nome"])):
-        if dados["Nome"][i] == nome:
-            for chave in dados.keys():
-                resultado += f"{dados[chave][i]} "
-            return resultado
-    resultado = "Não existe contato com esse nome."
+    resultado = dados[dados["Nome"] == nome]
+    if len(resultado) == 0:
+        resultado = "Não existe contato com esse nome."
     return resultado
 
+def hubUm(caminho):
+    dados = {
+        "Nome": [],
+        "Telefone": [],
+        "E-mail": [] 
+        }
+    resposta = "s"
+    i = 1
 
-dados = {
-    "Nome": [],
-    "Telefone": [],
-    "E-mail": [] 
-    }
-resposta = "s"
-i = 1
+    while resposta == "s":
+        dados["Nome"].append(input(f"Digite o nome do {i}º contato: "))
+        dados["Telefone"].append(input(f"Digite o telefone do {i}º contato: "))
+        dados["E-mail"].append(input(f"Digite o e-mail do {i}º contato: "))
+        i += 1
+        resposta = input("Digite (s) se desejas continuar a adicionar dados: ").lower()
 
-while resposta == "s":
-    dados["Nome"].append(input(f"Digite o nome do {i}º contato: "))
-    dados["Telefone"].append(input(f"Digite o telefone do {i}º contato: "))
-    dados["E-mail"].append(input(f"Digite o e-mail do {i}º contato: "))
+    escreverDadosArquivos(dados, caminho)
 
-    i += 1
-    resposta = input("Digite (s) se desejas continuar a adicionar dados: ").lower()
+def hubDois(caminho):
+    resposta = input("Digite (s) se desejas a adicionar contatos na agenda: ").lower()
+    i = 1
 
-escreverDadosArquivos(dados, caminho)
+    while resposta == "s":
+        nome = input(f"Digite o nome do {i}º contato: ")
+        telefone = input(f"Digite o telefone do {i}º contato: ")
+        email = input(f"Digite o e-mail do {i}º contato: ")
+        arquivo = open(caminho,"a")
+        arquivo.write(f"{nome},{telefone},{email}\n")
+        arquivo.close()
+        i += 1
+        resposta = input("Digite (s) se desejas continuar a adicionar dados: ").lower()
+
+if not os.path.exists(caminho):
+    hubUm(caminho)
+else:
+    hubDois(caminho)
 
 nome = input("Qual o nome do Contato que desejas procurar: ")
-print(buscarDadosArquivos(caminho, nome))
+print(buscarContatoNome(caminho, nome))
---
+++ algoritimos-e-programacao-de-computadores/spoj/ex001.py
@@ -0,0 +1,3 @@
+# https://br.spoj.com/problems/ESTAGIO/
+
+#  entrada uma lista de nomes e de médias finais dos alunos de uma turma, e determina o aluno com a maior média na turma
