a91752a9ee0e0ecdd8757078faa05b5de0db00a1
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-11-24 10:27:38 -0300

    up

--- estrutura-de-dados/aula01/exercicios/ex006.py
+++ estrutura-de-dados/aula01/exercicios/ex006.py
@@ -5,3 +5,15 @@
 #  Exercício 06:
 #  6. Algoritmo que recebe duas listas e imprime a intersecção das duas listas
 #  Função de Custo de Tempo deste Algoritmos:
+
+lista1 = [0,1,2,3,4,5,6,7,8]
+lista2 = [-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7]
+lista3 = []
+
+for numero1 in lista1:
+    for numero2 in lista2:
+        if numero1 == numero2:
+            lista3.append(numero1)
+            break
+
+print(lista3)
--- estrutura-de-dados/aula01/exercicios/ex007.py
+++ estrutura-de-dados/aula01/exercicios/ex007.py
@@ -5,3 +5,12 @@
 #  Exercício 07:
 #       7. Algoritmo que tenta quebrar uma senha de quatro dígitos numéricos
 #  Função de Custo de Tempo deste Algoritmos:
+
+senha = "0004"
+
+for tentativa in range(10000):
+    tentativa = str(tentativa)
+    tentativa = "0" * (4-len(tentativa)) + tentativa
+    if senha == tentativa:
+        print(f"A senha é {tentativa}")
+        break
