5346f5eb9c9621b33df83b37c8cac33cde280d8b
Jônatas Duarte <jonatasdvl@hotmail.com>
2023-12-07 11:14:15 -0300

    up

--- estrutura-de-dados/aula02/exercicios/ex001.py
+++ estrutura-de-dados/aula02/exercicios/ex001.py
@@ -3,3 +3,23 @@
 #  Professor: Rodolfo Carneiro Cavalcante
 #  Aluno: Jônatas Duarte Vital Leite
 #  Exercício 01: Implemente uma função recursiva que, dados dois números inteiros x e n, calcule o valor de x.n
+import random
+
+def multiplicacao(x, n):
+    if(n == 0 or x == 0):
+        return 0
+    elif (n != 0):
+        if(n < 0):
+            x = -x
+            n = -n
+        return x + multiplicacao(x, n-1)
+    return x
+
+# x = int(input("x: "))
+# n = int(input("n: "))
+
+x = random.randint(-10, 10)
+n = random.randint(-10, 10)
+
+print(x,"x",n,"=",multiplicacao(x, n))
+
--- estrutura-de-dados/aula02/exercicios/ex002.py
+++ estrutura-de-dados/aula02/exercicios/ex002.py
@@ -3,3 +3,31 @@
 #  Professor: Rodolfo Carneiro Cavalcante
 #  Aluno: Jônatas Duarte Vital Leite
 #  Exercício 02: Implemente uma função recursiva que, dados dois números inteiros x e n, calcule o valor de x^n
+
+import random
+
+def elevacao(x, n):
+    if (n == 0):
+        if (x == 0):
+            return "Inderterminação Matemática"
+        return 1
+    elif (n > 1):
+        return x * elevacao(x, n-1)
+    elif (n < -1):
+        n = -n
+        x * elevacao(x, n-1)
+        return 1 / elevacao(x, n-1)
+
+    return x
+
+
+# x = int(input("x: "))
+# n = int(input("n: "))
+
+x = random.randint(-10, 10)
+n = random.randint(-10, 10)
+
+print(f"{x} x {n} = {elevacao(x, n)}")
+
+
+#ainda tem um problema
