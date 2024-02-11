fb2e04afc04c9ff798b7492a3c2d8b4bfe6580e0
Jônatas Duarte <jonatasdvl@hotmail.com>
2024-02-11 17:53:47 -0300

    Update cda05.py
    Agora tá certo

--- estrutura-de-dados/aula04/cda/cda05.py
+++ estrutura-de-dados/aula04/cda/cda05.py
@@ -4,22 +4,29 @@ def compara(lista):
   i = 0
   j = len(lista)-1
   pivo = len(lista)//2
-  valorPivo = lista[pivo]
-  while j!=i and j > i:
+  while j > i: 
     if lista[j] > lista[pivo]:
       j -= 1
     elif lista[i] < lista[pivo]:
       i += 1
     else:
       lista[i], lista[j] = lista[j], lista[i] 
+      if pivo == i:
+        pivo = j
+      elif pivo == j:
+        pivo = i
       i += 1
       j -= 1
-  lista1, lista2 = lista[:i], lista[i:]
-  # if valorPivo != lista[pivo]:
-  #   lista1, lista2 = lista[:i], lista[i:]
-  #   print("oi")
-  # else:
-  #   lista1, lista2 = lista[:(len(lista)//2)], lista[(len(lista)//2):]
+
+  if lista[i] < lista[pivo]:
+    lista[pivo], lista[i] = lista[i], lista[pivo]
+    pivo = i+1
+  elif lista[j] > lista[pivo]:
+    pivo = j
+    lista[pivo], lista[j] = lista[j], lista[pivo]
+
+  lista1, lista2 = lista[:pivo], lista[pivo:]
+
   print(lista1,lista2)
   return lista1, lista2
 
@@ -35,6 +42,16 @@ def quickSort(lista):
 #     return quickSort(lista1), quickSort(lista2) 
 #   return lista
 
-lista = [3,5,8,4,2,1,9,0]
-# lista = [9,8,7,6,5,4,3,2,1,0]
-print(quickSort(lista))
+listaUm = [3,5,8,4,2,1,9,0]
+listaDois = [3,5,8,1,2,1,9,0]
+listaTres = [9,8,7,6,5,4,3,2,1,0]
+listaQuatro = [0,1,2,3,4,5,6,7,8,9]
+
+print(quickSort(listaUm))
+print("---")
+print(quickSort(listaDois))
+print("---")
+print(quickSort(listaTres))
+print("---")
+print(quickSort(listaQuatro))
+print("---")
