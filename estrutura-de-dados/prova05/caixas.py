1d074811b48ad7eff771e06a3bb3686c8f4574b0
Jônatas Duarte <jonatasdvl@hotmail.com>
2024-03-08 18:59:49 -0300

    create new resources, improve functions and make the system dynamic

---
+++ estrutura-de-dados/prova05/caixas.py
@@ -0,0 +1,108 @@
+class Caixa():
+  nome = None
+  aberto = True
+  numeroCaixa = None
+  cliente = {"numero": None, "nome": None}
+
+  def __init__(self, nome, numeroCaixa):
+    self.nome = nome
+    self.numeroCaixa = numeroCaixa
+  
+  def ocupado(self):
+    return self.cliente["numero"] != None
+
+  def chamar(self, cliente):
+    if self.ocupado():
+      print("Esse caixa já está ocupado!")
+    elif not(self.aberto):
+      print("Esse caixa já está fechado")
+    else:
+      self.cliente = cliente
+
+  def desocupar(self):
+    if self.ocupado():
+      numero = self.cliente["numero"]
+      nome = self.cliente["nome"]
+      self.cliente = {"numero": None, "nome": None}
+      return numero, nome
+    else:
+      print("Esse caixa já está desocupado!")
+
+  def fechar(self):
+    if self.aberto:
+      self.aberto = False
+    else:
+      print(f"Esse caixa já está fechado!")
+
+  def abrir(self):
+    if self.aberto == False:
+      self.aberto = True
+    else:
+      print(f"Esse caixa já está aberto!")
+  
+  def imprimir(self):
+    if not(self.aberto) and self.ocupado():
+      print(f"Caixa {self.numeroCaixa}: -{self.nome}- 'Fechado Temporariamente'")
+      numero = self.cliente["numero"]
+      nome = self.cliente["nome"]
+      print(f"Cliente: Nº {numero}, {nome}")
+    else:
+      print(f"Caixa {self.numeroCaixa}: -{self.nome}-")
+      if self.ocupado():
+        numero = self.cliente["numero"]
+        nome = self.cliente["nome"]
+        print(f"Cliente: Nº {numero}, {nome}")
+      elif not(self.aberto):
+        print("Fechado")
+      else:
+        print("Livre")
+      
+class Caixas():
+  caixas = []
+  quantidadeDeCaixa = 0
+
+  def __init__(self, quantidadeDeCaixa, modoTeste=False):
+    self. quantidadeDeCaixa = quantidadeDeCaixa
+    if modoTeste == False:
+      for i in range(1, quantidadeDeCaixa+1):
+        nomeAtendente = input(f"Digite o nome do atendente Nº 0{i}: ")
+        self.caixas.append(Caixa(nomeAtendente, f"0{i}"))
+    else:
+      self.caixas.append(Caixa("Rodolfo", "1"))
+      self.caixas.append(Caixa("Patrick", "2"))
+      self.caixas.append(Caixa("Raquel", "3"))
+
+  def contarAbertos(self):
+    count = 0
+    for caixa in self.caixas:
+      if caixa.aberto:
+        count += 1
+    return count
+  
+  def contarOcupados(self):
+    count = 0
+    for caixa in self.caixas:
+      if caixa.ocupado():
+        count += 1
+    return count
+
+  def imprimir(self):
+    for caixa in self.caixas:
+      caixa.imprimir()
+
+  def imprimirAtendentes(self, opcao=0):
+    if opcao == 1: 
+      for caixa in self.caixas:
+        if not(caixa.aberto):
+          print(f"Caixa Nº 0{caixa.numeroCaixa}: {caixa.nome}")
+    elif opcao == 2: 
+      for caixa in self.caixas:
+        if caixa.aberto:
+          print(f"Caixa Nº 0{caixa.numeroCaixa}: {caixa.nome}")
+    elif opcao == 0:
+      for caixa in self.caixas:
+        print(f"Caixa Nº 0{caixa.numeroCaixa}: {caixa.nome}")
+
+
+    # for i in range(self.quantidadeDeCaixa):
+    #   print(f"Caixa Nº 0{i+1}: {self.caixas[i].nome}")
--- estrutura-de-dados/prova05/sistema-de-atendimento-correios.py
+++ estrutura-de-dados/prova05/sistema-de-atendimento-correios.py
@@ -1,84 +1,135 @@
 from fila import Fila
-from atendente import Atendente
+from caixas import Caixas
 
 fila = Fila()
+modoTeste = True
 
+print("------------------------------------------")
 print("Bem-vindo ao sistema de atendimento dos Correios")
 print("------------------------------------------")
-quantidadeDeCaixa = int(input("Digite a quantidade de Atendente: "))
-if quantidadeDeCaixa > 9:
-  print("O máximo de caixas ativo é de 9")
-  quantidadeDeCaixa = 9
 
-caixas = []
-for i in range(1, quantidadeDeCaixa+1):
-  nomeAtendente = input(f"Digite o nome do atendente Nº 0{i}: ")
-  caixas.append(Atendente(nomeAtendente, f"{i}"))
+quantidadeDeCaixa = None
+
+if not(modoTeste):
+  while quantidadeDeCaixa == None:
+    try: 
+      quantidadeDeCaixa = int(input("Digite a quantidade de atendente: "))
+      if quantidadeDeCaixa > 9:
+        print("O máximo de caixas ativo é de 9")
+        quantidadeDeCaixa = 9
+    except ValueError:
+      print("Você digitou um valor inválido, tente novamente!")
+      quantidadeDeCaixa = None
+  print("------------------------------------------")
+else:
+  quantidadeDeCaixa = 3
+
+caixasObjeto = Caixas(quantidadeDeCaixa, modoTeste)
 
 print("Caixas Ativos:")
-for i in range(quantidadeDeCaixa):
-  print(f"Caixa Nº 0{i+1}: {caixas[i].nome}")
+caixasObjeto.imprimirAtendentes()
+
+caixas = caixasObjeto.caixas
 
 aberto = True
 i = 1
 
-while aberto or not(fila.vazia()) or caixas[0].ocupado() or caixas[1].ocupado() or caixas[2].ocupado():
+while aberto or not(fila.vazia()) or caixasObjeto.contarOcupados() != 0:
   print("------------------------------------------")
-  if caixas[0].ocupado(): 
-    print("1. Chamar o proximo para caixa 01;")
-  if caixas[1].ocupado():
-    print("2. Chamar o proximo para caixa 02;")
-  if caixas[2].ocupado():
-    print("3. Chamar o proximo para caixa 03;")
+  j  = 0
+  if caixasObjeto.contarOcupados() != 0: 
+    j += 1
+    print(f"{j}. Finalizar atendimento e provavelmente chamar o próximo da fila para o caixa;")
+  if caixasObjeto.contarAbertos() != quantidadeDeCaixa:
+    j += 1
+    print(f"{j}. Abrir caixa;")
+  if caixasObjeto.contarAbertos() != 0:
+    j += 1
+    print(f"{j}. Fechar caixa;")
   if aberto == True:
-    print("4. Adicionar um cliente;")
-    print("5. Fechar as portas do Correio;")
-  opcao = float(input("Digite o número de uma das opções acima: "))
+    j += 1
+    print(f"{j}. Adicionar um cliente;")
+    j += 1
+    print(f"{j}. Fechar as portas do Correio;")
+  try:
+    opcao = float(input("Digite o número de uma das opções acima: "))
+  except ValueError:
+    opcao = 1000
   print("------------------------------------------")
 
-  if opcao == 1 and caixas[0].ocupado():
-    numero, nome = caixas[0].desocupar()
-    print(f"Cliente {nome} de Nº {numero} saiu!")
-    if not(fila.vazia()):
-      numero, nome = fila.remover()
-      caixas[0].chamar({"nome": nome, "numero": numero})
-  elif opcao == 2 and caixas[1].ocupado():
-    numero, nome = caixas[1].desocupar()
-    print(f"Cliente {nome} de Nº {numero} saiu!")
-    if not(fila.vazia()):
-      numero, nome = fila.remover()
-      caixas[1].chamar({"nome": nome, "numero": numero})
-  elif opcao == 3 and caixas[2].ocupado():
-    numero, nome = caixas[2].desocupar()
-    print(f"Cliente {nome} de Nº {numero} saiu!")
-    if not(fila.vazia()):
-      numero, nome = fila.remover()
-      caixas[2].chamar({"nome": nome, "numero": numero})
-  elif opcao == 4 and aberto == True:
+  if ((opcao == j-2 or opcao == j-3 or opcao == j-4) and aberto == True) or aberto == False: 
+    if caixasObjeto.contarOcupados() != 0 and (opcao == j-4 and caixasObjeto.contarAbertos() != 0 and caixasObjeto.contarAbertos() != quantidadeDeCaixa or opcao == j-3 and (caixasObjeto.contarAbertos() == 0 or caixasObjeto.contarAbertos() == quantidadeDeCaixa) or aberto == False and opcao == j-2 and caixasObjeto.contarAbertos() != 0 and caixasObjeto.contarAbertos() != quantidadeDeCaixa or opcao == j-1 and (caixasObjeto.contarAbertos() == 0 or caixasObjeto.contarAbertos() == quantidadeDeCaixa)):
+      print("Caixas disponiveis finalizar o atendimento e provavelmente para chamar o próximo:")
+      for caixa in caixas:
+        if caixa.ocupado():
+          caixa.imprimir()          
+      j = "chamar"
+    elif caixasObjeto.contarAbertos() != quantidadeDeCaixa and (opcao == j-3 or opcao == j-2 and caixasObjeto.contarAbertos() == 0 or aberto == False and (opcao == j-1 or opcao == j and caixasObjeto.contarAbertos() == 0)): 
+      print(f"Caixas disponiveis para abrir caixa")
+      caixasObjeto.imprimirAtendentes(1)
+      j = "abrir"
+    else:
+      print(f"Caixas disponiveis para fechar caixa")
+      caixasObjeto.imprimirAtendentes(2)
+      j = "fechar"
+    try:
+      opcao = int(input("Digite o número um dos caixas acima: "))
+    except ValueError:
+      opcao = 1000
+    print("------------------------------------------")
+
+    # ajeitar
+      #     for caixa in caixas:
+      # if not(caixas.ocupado()):
+    
+    try:
+      caixas[opcao-1].ocupado()
+      cond = True
+    except ValueError:
+      cond = False
+
+    if j == "chamar" and cond and caixas[opcao-1].ocupado():  
+      numero, nome = caixas[opcao-1].desocupar()
+      print(f"Cliente {nome} de Nº {numero} saiu!")
+      if not(fila.vazia()):
+        numero, nome = fila.remover()
+        caixas[opcao-1].chamar({"nome": nome, "numero": numero})
+    elif j == "abrir" and cond and not(caixas[opcao-1].aberto):
+      caixas[opcao-1].abrir()
+      if fila.vazia():
+        print(f"O caixa de Nº {opcao} abriu!")
+      else:
+        numero, nome = fila.remover()
+        caixas[opcao-1].chamar({"nome": nome, "numero": numero})
+        print(f"Cliente {nome} de Nº {numero} saiu e o caixa de Nº {opcao} fechou!")
+    elif j == "fechar" and cond and caixas[opcao-1].aberto:
+      if caixas[opcao-1].ocupado():
+        numero, nome = caixas[opcao-1].desocupar()
+        print(f"Cliente {nome} de Nº {numero} saiu e o caixa de Nº {opcao} fechou!")
+      else:
+        print(f"O caixa de Nº {opcao} fechou!")
+      caixas[opcao-1].fechar()
+    else:
+      print("Valor Inválido")
+
+# ajeitado
+  elif opcao == j-1 and aberto == True:
     nome = input(f"Digite o nome do cliente de Nº {i}: ")
     if len(nome) >= 2:
       fila.inserir(i, nome)
     else:
       fila.inserir(i)
     i += 1 
-  elif opcao == 5 and aberto == True:
+    for caixa in caixas:
+      if not(caixa.ocupado()) and caixa.aberto:
+        numero, nome = fila.remover()
+        caixa.chamar({"nome": nome, "numero": numero})
+        break
+  elif opcao == j and aberto == True:
     print("As portas do Correio foram fechadas!")
     aberto = False
   else:
     print("Valor Inválido")
-  
-  if opcao == 4:
-    if not(caixas[0].ocupado()):
-      numero, nome = fila.remover()
-      caixas[0].chamar({"nome": nome, "numero": numero})
-    elif not(caixas[1].ocupado()):
-      numero, nome = fila.remover()
-      caixas[1].chamar({"nome": nome, "numero": numero})
-    elif not(caixas[2].ocupado()):
-      numero, nome = fila.remover()
-      caixas[2].chamar({"nome": nome, "numero": numero})
 
-  caixas[0].imprimir()
-  caixas[1].imprimir()
-  caixas[2].imprimir()
+  caixasObjeto.imprimir()
   fila.imprimir()
